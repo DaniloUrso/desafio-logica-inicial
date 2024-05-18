72f702bb74ef81d663305129c7d0aec4f05b5b40
DaniloUrso <danteles.santos@gmail.com>
2024-05-18 16:05:53 -0300

    Esse é o meu segundo commit, Aqui iniciei o desafio, já acrescentando as primeiras linhas.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+console.log("Hello World!")
+console.log("Vamos iniciar o Desafio do Felipão!")
+console.log("Começa agora a aventura do Desafio Classificador de nível de Herói ")
+
+let heroi = "Ursaum13"
+let xp = 900
+
+
+
+console.log("O nosso herói " + heroi + " possui a quantidade de " + xp + " XP")
